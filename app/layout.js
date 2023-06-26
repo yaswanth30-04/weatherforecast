85f8b80b6be1e912ca62f769d9407ffd6d94afea
yaswanth30-04 <116426380+yaswanth30-04@users.noreply.github.com>
2023-06-26 17:11:59 +0530

    mixed content fix

--- app/layout.js
+++ app/layout.js
@@ -16,6 +16,7 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
       <body className={`${inter.className} `} >{children}</body>
     </html>
   )
